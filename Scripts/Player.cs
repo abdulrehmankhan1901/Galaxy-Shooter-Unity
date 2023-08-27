8f5d93ebaec1fd26d31038804234dd438b08351e
abdulrehmankhan1901 <98641285+abdulrehmankhan1901@users.noreply.github.com>
2023-08-27 13:16:01 +0500

    Added Shooting

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,11 +8,19 @@ public class Player : MonoBehaviour
     // player variables
     [SerializeField]    // allows you to edit private variables at runtime in the Unity Game Engine
     private int _speed = 5;
+    [SerializeField]
+    private GameObject _laserPrefab;
     private Vector3 _direction = Vector3.zero;
 
+    // firing variables
+    [SerializeField]
+    private float _spawnLaserOffset = 0.8f;
+    private float _canFire = -1f;
+    public float fireCoolDown = 0.2f;
+
     // world bound variables
-    private float x_bounds = 11.3f; // the player will wrap around; therefore only need one
-    private float y_bounds = -3.8f; // the positive bounds for y-axis is 0
+    private float _x_bounds = 11.3f; // the player will wrap around; therefore only need one
+    private float _y_bounds = -3.8f; // the positive bounds for y-axis is 0
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +34,12 @@ public class Player : MonoBehaviour
     {
         // player movement
         MovePlayer();
-        
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire) // check if there is input and the cool down has ended
+        {
+            FireLaser(fireCoolDown);
+        }
+
     }
 
     void MovePlayer()
@@ -47,17 +60,26 @@ public class Player : MonoBehaviour
     void LimitPlayerToWorldBounds()
     {
         // limit player in the y-axis (till the center of the screen because enemies will come from the top of the screen)
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, y_bounds, 0), 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _y_bounds, 0), 0);
 
         // wrap the player around the x-axis (teleport the player to the opposite side if the player goes out of bounds)
-        if (transform.position.x > x_bounds)
+        if (transform.position.x > _x_bounds)
         {
-            transform.position = new Vector3(-x_bounds, transform.position.y, 0);
+            transform.position = new Vector3(-_x_bounds, transform.position.y, 0);
         }
-        else if (transform.position.x < -x_bounds)
+        else if (transform.position.x < -_x_bounds)
         {
-            transform.position = new Vector3(x_bounds, transform.position.y, 0);
+            transform.position = new Vector3(_x_bounds, transform.position.y, 0);
         }
+    }
+
+    void FireLaser(float coolDown)
+    {
+        // implement the cool down time
+        _canFire = Time.time + coolDown;
+
+        // Quaternion refers to the rotation of the gameObject and ".identity", in the simplest of terms, refers to the default rotation
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, _spawnLaserOffset, 0), Quaternion.identity);
     }
 
 }
